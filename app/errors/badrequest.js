266ad1dfd1200812a76e27fc65affa5306eacfb3
shubhampatwa <shubhampatwa526@gmail.com>
2018-08-03 18:00:44 +0530

    Added errors handler.

---
+++ app/errors/badrequest.js
@@ -0,0 +1,11 @@
+const BaseError = require('./base');
+
+class BadRequestError extends BaseError {
+
+  constructor(message) {
+    super(message, 400);
+  }
+
+}
+
+module.exports =  BadRequestError;
